8070b9f562599ff1038780cd9053fbca98ea9a3b
yeminhui <1051364497@qq.com>
2021-11-21 10:15:14 +0800

    尚硅谷springcloud学习

---
+++ cloud-config-client-3355/src/main/java/cn/yml/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,18 @@
+package cn.yml.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ConfigClientController {
+    @Value("${config.info}")
+    public String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
